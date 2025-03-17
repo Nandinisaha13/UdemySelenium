2d219a819f6967e51ccf4fe024b97d67514fd30a
nandinisaha13 <nandinisaha13@gmail.com>
2025-03-17 19:49:34 +0530

    my first commit

---
+++ src/main/java/nandinitest/App.java
@@ -0,0 +1,26 @@
+package nandinitest;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+        
+
+
+    
+   public static void main(String[] args) {
+    String expected = "Hello";
+        String actual = "Hello";
+        
+        //Assert.assertEquals(actual, expected, "Values do not match!");
+        System.out.println("Test Passed!");
+    
+   }
+    
+
+
+   
+}
