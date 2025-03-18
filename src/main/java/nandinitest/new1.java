d131f8da8c477e19e5ab4ee726200738f365bf6e
nandinisaha13 <nandinisaha13@gmail.com>
2025-03-18 09:38:58 +0530

    changed pom xml to support testng

---
+++ src/main/java/nandinitest/new1.java
@@ -0,0 +1,15 @@
+package nandinitest;
+
+import org.testng.annotations.Test;
+
+public class new1 {
+    public static void main(String[] args) {
+        System.out.println("check");
+        System.out.println("hello");
+    }
+    @Test
+    public void hello()
+    {
+        System.out.println("pello");
+    }
+}
